4f5633834183f28e0b02b1f76b77f6677c717923
dawnordark <55791638+dawnordark@users.noreply.github.com>
2025-07-28 06:58:46 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ PERIOD_MINUTES = {
 }
 
 RESISTANCE_INTERVALS = ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d', '3d', '1w', '1M']
-ALL_PERIODS = ['5m', '15m', '30m', '1h', '2h', '4极速赛车开奖网
+ALL_PERIODS = ['5m', '15m', '30m', '1h', '2h', '4h', '6h', '12h', '1d', '3d', '1w', '1M']
 def init_db():
     try:
         logger.debug("🛠️ 开始初始化数据库...")
